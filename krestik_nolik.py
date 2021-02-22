8e2ab2e2ff857db3dfd4d33aae02218458f76501
SergeyL-1979 <65560756+SergeyL-1979@users.noreply.github.com>
2021-02-22 17:28:36 +0300

    Update krestik_nolik.py

--- krestik_nolik.py
+++ krestik_nolik.py
@@ -1,88 +1,55 @@
-def greet():
-    print("===================")
-    print("  крестики-нолики  ")
-    print("===================")
-    print(" формат ввода: x y ")
-    print(" x - номер строки  ")
-    print(" y - номер столбца ")
+board = list(range(1,10))
 
+def draw_board(board):
+    print ("-" * 13)
+    for i in range(3):
+        print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+        print ("-" * 13)
 
-def show():
-    print()
-    print("    | 0 | 1 | 2 | ")
-    print("  --------------- ")
-    for i, row in enumerate(field):
-        row_str = f"  {i} | {' | '.join(row)} | "
-        print(row_str)
-        print("  --------------- ")
-    print()
-
-
-def ask():
-    while True:
-        cords = input("         Ваш ход: ").split()
-
-        if len(cords) != 2:
-            print(" Введите 2 координаты! ")
+def take_input(player_token):
+    valid = False
+    while not valid:
+        player_answer = input("Куда поставим " + player_token+"? ")
+        try:
+            player_answer = int(player_answer)
+        except:
+            print ("Некорректный ввод. Вы уверены, что ввели число?")
             continue
+        if player_answer >= 1 and player_answer <= 9:
+            if (str(board[player_answer-1]) not in "XO"):
+                board[player_answer-1] = player_token
+                valid = True
+            else:
+                print ("Эта клеточка уже занята")
+        else:
+            print ("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
 
-        x, y = cords
-
-        if not (x.isdigit()) or not (y.isdigit()):
-            print(" Введите числа! ")
-            continue
-
-        x, y = int(x), int(y)
-
-        if 0 > x or x > 2 or 0 > y or y > 2:
-            print(" Координаты вне диапазона! ")
-            continue
-
-        if field[x][y] != " ":
-            print(" Клетка занята! ")
-            continue
-
-        return x, y
-
-
-def check_win():
-    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
-                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
-                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
-    for cord in win_cord:
-        symbols = []
-        for c in cord:
-            symbols.append(field[c[0]][c[1]])
-        if symbols == ["X", "X", "X"]:
-            print("Выиграл X!!!")
-            return True
-        if symbols == ["0", "0", "0"]:
-            print("Выиграл 0!!!")
-            return True
+def check_win(board):
+    win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+    for each in win_coord:
+        if board[each[0]] == board[each[1]] == board[each[2]]:
+            return board[each[0]]
     return False
 
+def main(board):
+    counter = 0
+    win = False
+    while not win:
+        draw_board(board)
+        if counter % 2 == 0:
+            take_input("X")
+        else:
+            take_input("O")
+        counter += 1
+        if counter > 4:
+            tmp = check_win(board)
+            if tmp:
+                print (tmp, "выиграл!")
+                win = True
+                break
+        if counter == 9:
+            print ("Ничья!")
+            break
+    draw_board(board)
 
-greet()
-field = [[" "] * 3 for i in range(3)]
-count = 0
-while True:
-    count += 1
-    show()
-    if count % 2 == 1:
-        print(" Ходит крестик!")
-    else:
-        print(" Ходит нолик!")
-
-    x, y = ask()
-
-    if count % 2 == 1:
-        field[x][y] = "X"
-    else:
-        field[x][y] = "0"
-
-    if check_win():
-        break
-
-    if count == 9:
-        print(" Ничья!")
-        break
+main(board)
